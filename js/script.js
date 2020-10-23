eea0f248c9b834ff7adacb6e9c78afe8e29b5611
jared Long <jlong4223@gmail.com>
2020-10-23 16:42:14 -0500

    finished JS functions and project

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ const Base_URL = 'https://pokeapi.co/api/v2/pokemon/'
 
 
 //State Date
-let pokemonData; 
+let pokemonData, pokemonDetail; 
 
 
 //Cached element references 
@@ -34,13 +34,31 @@ function init() {
     getData()
 }
 
-function getData() {
+function getData(detailURL) {
+    console.log('detailURL', detailURL)
+    //declare a local variable to take whichever url we need 
+    let url; 
+    if(detailURL === undefined) {
+        //we want all the pokemon
+        url = Base_URL
+    } else {
+        // we want a single pokemon
+        url = detailURL;
+    }
     //fetch data using AJAX
-    $.ajax(Base_URL).then(function(data){
+    $.ajax(url).then(function(data){
         //take the returned data and assigned it to global state variable
-         pokemonData = data;
-         //call render to visualize it to the DOM
-         render();
+        // the below if is getting all the pokemon
+         if(detailURL === undefined){
+            pokemonData = data;
+            //call render to visualize it to the DOM
+            render();         
+         } else {
+             pokemonDetail = data; 
+             //call render and tell it 
+             render(true)
+         }
+         
     }, function(error) {
         console.log('Error: ', error)
     })
@@ -48,18 +66,34 @@ function getData() {
 
 
 function handleClick() {
-    alert('a card was clicked')
+   getData(this.dataset.url)
 }
 
-function render() {
-    // map over the objects inside of the pokemonData results array
-    //dynamically generate html for each element in the array 
-    //add that html to our collection element 
+function render(showModal) {
+    if(showModal === true){
+        //show the modal
+        //generate the html for the inner conent for the modal
+        //call the modal function on the modal element
+        const $modalContent = $(`
+            <img src="${pokemonDetail.sprites.front_default}"/>
+            <h5>${pokemonDetail.name}</h5>
+            <p>Height:${pokemonDetail.height}</p>
+            <p>Moves:${pokemonDetail.moves.length}</p>
+            <p>abilities: ${pokemonDetail.abilities.length}</p>
+        `)
+        const $modal = $('#pokemodal')
+        $modal.html($modalContent)
+        $modal.modal()
+    } else{
+        // map over the objects inside of the pokemonData results array
+         //dynamically generate html for each element in the array 
+         //add that html to our collection element 
     const htmlArray = pokemonData.results.map((pokemon) => {
         return`
-        <article class="card flex-ctr">
+        <article data-url="${pokemon.url}" class="card flex-ctr">
             <h3>${pokemon.name}</h3>
         </article>`;
     } )
     $collection.html(htmlArray)
+    }
 }
